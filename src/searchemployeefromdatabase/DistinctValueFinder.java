b306de65ab9ba552ac8d9994131618dd4760c97a
ahmet <ahmetduser2000@gmail.com>
2020-09-15 00:20:27 +0400

    variable name changes - naming convention

--- src/searchemployeefromdatabase/DistinctValueFinder.java
+++ src/searchemployeefromdatabase/DistinctValueFinder.java
@@ -9,26 +9,26 @@ import java.util.ArrayList;
 public class DistinctValueFinder {
 
     public ArrayList<String> getListOfDistinctPositionValues(String query) {
-        ArrayList<String> distinctPosition = new ArrayList<>();
-        getList(query, distinctPosition, "Position");
-        return distinctPosition;
+        ArrayList<String> distinctPositionList = new ArrayList<>();
+        getList(query, distinctPositionList, "Position");
+        return distinctPositionList;
     }
 
     public ArrayList<String> getListOfDistinctBranchIDValues(String query) {
-        ArrayList<String> distinctBranchID = new ArrayList<>();
-        getList(query, distinctBranchID, "BranchID");
+        ArrayList<String> distinctBranchIDList = new ArrayList<>();
+        getList(query, distinctBranchIDList, "BranchID");
 
-        return distinctBranchID;
+        return distinctBranchIDList;
     }
 
-    private void getList(String query, ArrayList<String> distinctPosition, String field) {
+    private void getList(String query, ArrayList<String> distinctValues, String field) {
         try (Connection connection = DataBaseConnector.getDBConnection()) {
             Statement statement = connection.createStatement();
 
             ResultSet results = statement.executeQuery(query);
             while (results.next()) {
-                String employeePosition = results.getString(field);
-                distinctPosition.add(employeePosition);
+                String val = results.getString(field);
+                distinctValues.add(val);
             }
         } catch (SQLException e) {
             System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
